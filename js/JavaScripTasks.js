ff3efd6d50b3dac2642aaa99cfc302bf445ba750
Dmytro <syryi.dmytro@gmail.com>
2022-12-20 20:12:35 +0100

    Practice Function

--- js/JavaScripTasks.js
+++ js/JavaScripTasks.js
@@ -3,44 +3,44 @@
 // ЗАДАНИЯ!!!
 
 
-// for (let i = 5; i < 11; i++){
-//     console.log (i);
-// }
-// for (let i = 20; i >= 10; --i){
-//     if (i === 12) break;
-//     console.log (i);
-// }
-// for (let i = 2; i <= 10; i++){   
-//     if(i%3 === 0) continue;
+for (let i = 5; i < 11; i++){
+    console.log (i);
+}
+for (let i = 20; i >= 10; --i){
+    if (i === 12) break;
+    console.log (i);
+}
+for (let i = 2; i <= 10; i++){   
+    if(i%3 === 0) continue;
  
-//     console.log (i);
-// }
+    console.log (i);
+}
  
-//  let num = 1;
-//   while ( num <= 10) {
-//     num++;      
-//     if(num === 3) continue;
-//     if(num=== 5) continue;
-//     if(num === 7) continue;
-//     if(num === 9) continue;
-//     console.log (num);
-//     num++;
-//  }
+ let num = 1;
+  while ( num <= 10) {
+    num++;      
+    if(num === 3) continue;
+    if(num=== 5) continue;
+    if(num === 7) continue;
+    if(num === 9) continue;
+    console.log (num);
+    num++;
+ }
 
  
-//  for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-// let num = 2;
-//   while ( num < 16) {
-//     num++;      
-//     if(num%2 === 0) continue;
-//     console.log (num);
-//     }
+ for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+let num = 2;
+  while ( num < 16) {
+    num++;      
+    if(num%2 === 0) continue;
+    console.log (num);
+    }
 
 
     {
@@ -65,7 +65,7 @@
     return result;
 
 
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
+ 
 
 
 
@@ -98,3 +98,22 @@
     }
     getMathResult(10,5);
     console.log(getMathResult);
+
+
+
+//     Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+function secondTask(){
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    for (let i = 1; i < data.length; i++){
+        if (typeof(data) === "number"){
+            data [i] = data [i]*2;
+        }
+     else if (typeof(data) === "string"){
+        return  `${data} - done!`;
+    }
+}
+}
+console.log(secondTask);
--- js/JavaScriptPractice.js
+++ js/JavaScriptPractice.js
@@ -7,49 +7,66 @@
 
 
 
-const numberOfFilms = prompt ("Сколько фильмов вы уже просмотрели?",'');
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt ("Сколько фильмов вы уже просмотрели?",'');
+    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt ("Сколько фильмов вы уже просмотрели?",'');
+    }
+}
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    gender: [],
+    genres: [],
     privat: false
 };
 
-for (let i = 0; i < 2; i++){
-    const a = prompt ("Один из последних просмотреных фильмов?",''),
-          b = prompt ("На сколько оцените его?",'');
-    
-    if (a != null && b != null && a != '' && b != '' && a.length <50){
-    personalMovieDB. movies [a] = b;
-    console.log ('done');}
-    else{
-        console.log ("error");
-        i--;
-    }
-}
-if (personalMovieDB.count < 10 ){
-    console.log('мало');
-}else if (personalMovieDB.count >=10 && personalMovieDB.count < 30){
-    console.log('достаточно');
-}else if (personalMovieDB.count >= 30){
-    console.log('много');
-}else {
-    console.log('ошибка');
-}
 
-
-let num = 0;
-while (num < 2){
-    num++;
-    const a = prompt ("Один из последних просмотреных фильмов?",''),
-          b = prompt ("На сколько оцените его?",'');
-    if (a != null && b != null && a != '' && b != '' && a.length <50){
-            personalMovieDB. movies [a] = b;
-            console.log ('done');}
-    else{
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++){
+        const a = prompt ("Один из последних просмотреных фильмов?",''),
+              b = prompt ("На сколько оцените его?",'');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length <50){
+        personalMovieDB. movies [a] = b;
+        console.log ('done');}
+        else{
             console.log ("error");
             i--;
-            }
+        }
+    }
 
 }
+rememberMyFilms();
+
+
+function detectPeronalLevel(){
+    if (personalMovieDB.count < 10 ){
+        console.log('мало');
+    }else if (personalMovieDB.count >=10 && personalMovieDB.count < 30){
+        console.log('достаточно');
+    }else if (personalMovieDB.count >= 30){
+        console.log('много');
+    }else {
+        console.log('ошибка');
+    }
+}
+detectPeronalLevel();
+
+function C(hidden){
+    if(!hidden){
+        console.log(personalMovieDB);
+    }
+}
+detectPeronalLevel(personalMovieDB.privat);
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i - 1] = prompt (`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
