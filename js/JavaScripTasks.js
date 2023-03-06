06f53c399c3db02a2438124959ef3802d043bab9
Dmytro Syryi <syryi.dmytro@gmail.com>
2023-03-06 20:53:29 +0100

    aaa

--- js/JavaScripTasks.js
+++ js/JavaScripTasks.js
@@ -1,1 +1,12 @@
 "use strict";
+function factorial(n) {
+    if (!Number.isInteger(n) || typeof n !== "number") {
+      return "Ошибка, проверьте данные";
+    }
+    if (n <= 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  console.log(factorial(5));
